aaddaf4bb5a1de9aced10845df6d2608b9b567e8
porcel1910 <porcel_augusto@hotmail.com>
2021-08-04 19:53:56 -0300

    implementado data e hora
     foi  implementado data e hora   no   display da calculadora

---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,72 @@
+class CalcController{
+//anderline e pivado ==  this._dataAtual;
+    constructor(){
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl =  document.querySelector("#data");
+        this._timeEl =  document.querySelector("#hora");       
+        this.currentDate;
+        this.initialize();
+
+    }
+
+    initialize(){
+        this.setDisplayDateTime();
+         setInterval(()=>{
+
+            this.setDisplayDateTime();
+             
+
+
+        },1000  );
+
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{
+            day:"2-digit",month:"long",year:"numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale); 
+
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+
+    }
+
+    set displayTime(value){
+         this._timeEl.innerHTML = value;
+
+    }
+
+    get displayDate(){
+       return this._dateEl.innerHTML;
+
+    }
+
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+
+    }
+
+    get displayCalc(){
+         return this._displayCalcEl.innerHTML;
+    }
+    
+    set displayCalc(value){
+
+        this._displayCalcEl.innerHTML = value;
+    }
+
+    get currentDate(){
+        return new Date();
+   }
+   
+   set currentDate(value){
+
+       this.currentDate = value;
+   }
+
+
+}
