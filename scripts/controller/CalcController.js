eac1aece0b568e4cde48bb1351723a6b0251b354
porcel1910 <porcel_augusto@hotmail.com>
2021-08-05 19:45:10 -0300

    switch
     foi implementado  switch na classe calController mais funções

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,12 +1,14 @@
 class CalcController{
 //anderline e pivado ==  this._dataAtual;
     constructor(){
+        this._operation =[];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl =  document.querySelector("#data");
         this._timeEl =  document.querySelector("#hora");       
         this.currentDate;
         this.initialize();
+        this.initButtonsEvensts();
 
     }
 
@@ -15,10 +17,103 @@ class CalcController{
          setInterval(()=>{
 
             this.setDisplayDateTime();
-             
+         },1000  );
+    }
+    addEventListenerAll(element,events,fn){
 
+        events.split (' ').forEach(event =>{
 
-        },1000  );
+                element.addEventListener(event, fn, false);
+        });
+
+    }
+    clearAll(){
+
+        this._operation =[];
+
+    }
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+    addOperation(value){
+
+        this._operation.push(value);
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+    execBtn(value){
+
+        switch (value) {
+            case 'ac':
+                this.clearAll();    
+                break;
+            case 'ce':
+                this.clearEntry();    
+                break;
+            case 'soma':
+                   
+                break;
+            case 'subritacao':
+                
+                break;
+            case 'divisao':
+                
+                break;
+            case 'multiplicacao':
+                   
+                break;
+            case 'porcento':
+              
+                break;
+            case 'igual':
+                
+                break;    
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':   
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));       
+                break;
+        
+           default:
+                this.setError();
+                break;
+        }
+    }
+
+    initButtonsEvensts(){
+
+       let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+       buttons.forEach((btn, index)=>{
+            this.addEventListenerAll(btn,"click drag", e =>{
+
+                let txtBtn = btn.className.baseVal.replace("btn-","");
+                this.execBtn(txtBtn);
+
+            });
+        
+            this.addEventListenerAll(btn,"mouseover mouseup mousedown", e =>{
+                btn.style.cursor = "pointer";
+            });
+
+       });
+
+       
 
     }
 
