6274ce21d332a7c4bfbdf295d43487bc196e9e64
porcel1910 <porcel_augusto@hotmail.com>
2021-08-11 19:26:33 -0300

    Aula 14
    Clicando mais de uma vez no igual   foi tratado o bug do botão igual

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -3,8 +3,8 @@ class CalcController{
     constructor(){
 
 
-        this._lastOperator ='';  
-        this._lastNumber ='';      
+        this._lastOperator = '';  
+        this._lastNumber = '';      
         this._operation =[];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -85,6 +85,7 @@ class CalcController{
 
     getResult(){
 
+       
         return eval(this._operation.join(""));
 
 
@@ -93,15 +94,28 @@ class CalcController{
     calc(){
 
         let last = '';
+        this._lastOperator = this.getLastItem();
 
-        if (this._operation.length > 3 ) {
-            last = this._operation.pop();
-            let result = this.getResult();
+        if (this._operation.length < 3 ){
 
+            let firstItem = this._operation[0];
+            this._operation= [firstItem, this._lastOperator,this._lastNumber];
 
         }
 
-    
+
+        if (this._operation.length > 3 ) {
+
+            last = this._operation.pop();
+            this._lastNumber =this.getResult();
+
+        } else if (this._operation.length == 3) {
+
+           
+            this._lastNumber =this.getLastItem(false); 
+
+        }
+     
         let result = this.getResult();
 
         if (last == '%') {
@@ -109,36 +123,45 @@ class CalcController{
            result /= 100;
            this._operation =[result];
 
-            
         } else {
         
-           
             this._operation =[result];
 
            if(last) this._operation.push(last); 
 
-
         }
 
-
-
-      
         this.setLastNumberToDisplay();
         
-
     }
-    setLastNumberToDisplay(){
-        let lastNumber;
+    getLastItem(isOperation = true){
+
+        let lastItem;
 
         for ( let i =this._operation.length-1; i >= 0 ; i--){
 
-            if (!this.isOperation(this._operation[i])) {
-                
-                lastNumber = this._operation[i];
+            if (this.isOperation(this._operation[i])  == isOperation) {
+                    
+                lastItem = this._operation[i];
                 break;
             }
 
         }
+        if (! lastItem) {
+
+            lastItem = (isOperation) ? this._lastOperator : this._lastNumber;
+            
+        }
+
+        return lastItem;
+
+    }
+
+
+    setLastNumberToDisplay(){
+        let lastNumber = this.getLastItem(false);
+
+       
         if (!lastNumber) lastNumber = 0;  
 
         this.displayCalc = lastNumber;
@@ -146,8 +169,6 @@ class CalcController{
     }
 
     addOperation(value){
-
-        
 
         if (isNaN(this.getLastOperation())) {
 
