ac4924a6aa818c0f7f2e374fb5f0d5ae4b27392c
porcel1910 <porcel_augusto@hotmail.com>
2021-08-12 19:59:28 -0300

    aula 19
     Foi implementado  o teclado  na calculadora  e  áudio

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,7 +2,8 @@ class CalcController{
 //anderline e pivado ==  this._dataAtual;
     constructor(){
 
-
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff= false;
         this._lastOperator = '';  
         this._lastNumber = '';      
         this._operation =[];
@@ -13,8 +14,44 @@ class CalcController{
         this.currentDate;
         this.initialize();
         this.initButtonsEvensts();
+        this.initKeyboard();
+       // this.copyToClipboard();
 
     }
+
+    copyToClipboard(){
+
+        let input = document.createElement('input');
+
+        input.value =  this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+        document.execCommand("Copy");
+
+        input.remove();
+
+
+    }
+    pasteFromClipboard(){
+
+        document.addEventListener('paste', e=>{
+
+        let text = e.clipboardData.getData('Text');
+        this.displayCalc = parseFloat(text);
+        
+        
+        console.log(text);
+
+
+        });
+
+
+
+    }
+
+
 
     initialize(){
         this.setDisplayDateTime();
@@ -23,6 +60,92 @@ class CalcController{
             this.setDisplayDateTime();
          },1000  );
          this.setLastNumberToDisplay();
+         this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn =>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+
+            });
+
+        });
+        
+    }
+
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+       if(this._audioOnOff){
+
+        this._audio.currentTime =0;
+        this._audio.play();
+       }
+
+
+    }
+
+    initKeyboard(){
+
+        document.addEventListener('keyup', e=>{
+
+            this.playAudio();
+
+            console.log(e.key);
+            switch (e.key) {
+                case 'Escape':
+                    this.clearAll();    
+                    break;
+                case 'Backspace':
+                    this.clearEntry();    
+                    break;
+                case '+':
+                case'-':
+                case'*':
+                case'/':
+                case'%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break; 
+                    
+                case ',':
+                case '.':
+                    this.addDot('.');
+                    
+                    break;
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':   
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));       
+                    break;
+                case 'c':
+                    if(e.ctrlKey)this.copyToClipboard();
+
+                break;
+            
+            }
+
+
+        });
+
+
+
     }
 
 
@@ -233,6 +356,8 @@ class CalcController{
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value) {
             case 'ac':
